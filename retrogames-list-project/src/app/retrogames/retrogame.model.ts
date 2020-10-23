5c3a0977ff25de6119de4fecfc2d1743026b895d
unknown <vatsop52@gmail.com>
2020-10-23 23:10:03 +0300

    adding game info to retrogames

--- retrogames-list-project/src/app/retrogames/retrogame.model.ts
+++ retrogames-list-project/src/app/retrogames/retrogame.model.ts
@@ -1,13 +1,15 @@
-import { stringify } from 'querystring';
+import { GameInfo } from '../shared/info.model';
 
 export class Retrogame {
    public name: string;
    public description: string;
    public imagePath: string;
+   public info: GameInfo[];
 
-    constructor(name: string, desc: string, imagePath: string) {
+    constructor(name: string, desc: string, imagePath: string, info: GameInfo[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
-    }
+        this.info = info;
+      }
 }

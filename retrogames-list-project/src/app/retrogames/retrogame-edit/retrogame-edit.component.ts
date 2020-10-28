7d55b726e0580378dab56b0023584daa564243aa
unknown <vatsop52@gmail.com>
2020-10-28 22:08:38 +0200

    retreiving route parameters

--- retrogames-list-project/src/app/retrogames/retrogame-edit/retrogame-edit.component.ts
+++ retrogames-list-project/src/app/retrogames/retrogame-edit/retrogame-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-retrogame-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retrogame-edit.component.css']
 })
 export class RetrogameEditComponent implements OnInit {
+  id: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params
+     .subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.editMode = params['id'] != null;
+      }
+     );
   }
 
 }
